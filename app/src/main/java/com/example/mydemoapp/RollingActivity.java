366c76d9f92406871174748f93e35253d88987eb
Nicholas Dimitriou <skeleynick@gmail.com>
2025-11-12 15:18:37 -0800

    logout

--- app/src/main/java/com/example/mydemoapp/RollingActivity.java
+++ app/src/main/java/com/example/mydemoapp/RollingActivity.java
@@ -6,10 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 /**
  * the page to roll on
  * Users can: roll to add to thier collection
